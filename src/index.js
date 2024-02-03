4da3212bfd514c65e187d37744e8642d9ef69f97
minbo <smb5174@naver.com>
2024-02-03 12:53:25 +0900

    feat: 가위바위보 결과 출력

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import ReactDOM from 'react-dom';
 
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
+
 ReactDOM.render(
   <>
-    <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
-  </>
-, document.getElementById('root')
+    <h1>가위바위보</h1>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+  </>,
+  document.getElementById('root')
 );
