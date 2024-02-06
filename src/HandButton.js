5948e3119cbf723d899f0562deee7ed12fa2a9ad
minbo <smb5174@naver.com>
2024-02-06 11:19:05 +0900

    feat: HandButton.js 파일의 button 태그에 인라인 스타일을 적용했다.

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,9 +1,25 @@
 import HandIcon from './HandIcon';
+import backgroundImg from './assets/purple.svg';
 
+const style = {
+  width: '166px',
+  height: '166px',
+  border: 'none',
+  outline: 'none',
+  textAlign: 'center',
+  cursor: 'pointer',
+  backgroundColor: 'transparent',
+  backgroundImage: `url(${backgroundImg})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'contain',
+};
+
+// 인라인 스타일을 적용해 주세요
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
-    <button onClick={handleClick}>
+    <button style={style} onClick={handleClick}>
       <HandIcon value={value} />
     </button>
   );
